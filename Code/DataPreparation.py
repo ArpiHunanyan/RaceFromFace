c6d17aecda6efca09756e3c6fe9bb31e9a64c55d
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-04-13 21:38:03 +0400

    Update DataPreparation.py

--- Code/DataPreparation.py
+++ Code/DataPreparation.py
@@ -1,5 +1,4 @@
 
-from pkgutil import get_data
 import pandas as pd
 import numpy as np
 import re
@@ -9,33 +8,21 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-#print(pd.__version__)
-def getValidation():
-    path = "Data/fairface_label_val.csv"
-    suffix = "Data/val/" 
-    return getData(path, suffix)
+def getValidation(M = 10954 - 1):
+    path_y = "Data/fairface_label_val.csv"
+    path_data = "Data/validation_data.npy"
+    return getData(path_y , path_data, M)
 
-def getTrain():
-    path = "Data/fairface_label_train.csv"
-    suffix = "Data/train/" 
-    return getData(path, suffix)
+def getTrain(M = 86744 - 1):
+    path_y = "Data/fairface_label_train.csv"
+    path_data = "Data/train_data.npy" 
+    return getData(path_y , path_data, M)
 
-def getData(path, suffix):
-    M = 30
-    yy = pd.read_csv(path, nrows = M )
-    y_index = [re.split('/', i)[1] for i in yy["file"]]
+def getData(path_y , path_data, M):
 
-        
-    data = np.empty((yy.shape[0], 224, 224, 3))
-    index = np.array([])
-        
-    for i in range(yy.shape[0]):
-
-        img = Image.open( suffix + y_index[i] )
-        index = np.append(index, yy["file"][i]) 
-        imageToMatrice = np.asarray(img)
-        data[i] = imageToMatrice
-
+    y = pd.read_csv(path_y, nrows = M , usecols = ["race"])
+    print()
+    assert len(y.race.unique()) == 7, 'InvalidRaceGroupSize'
 
     cleanup_nums = {"race": {
                                 'East Asian': 0,
@@ -48,10 +35,12 @@ def getData(path, suffix):
                             }
                     }
         
-    yy = yy.replace(cleanup_nums)
-    y =  to_categorical(yy["race"])
+    y = y.replace(cleanup_nums)
+    y =  to_categorical(y["race"])
+    data = np.load(path_data, mmap_mode='r', )
+    data = data[: M]
 
-    
+
     return (data, y)
 
 
