35a362d966fe2584961012218b58bf35dcd60919
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-04-23 09:23:55 +0400

    Update Demo.py

--- Code/Demo.py
+++ Code/Demo.py
@@ -1,40 +1,35 @@
-
 from tensorflow import keras
-from  DataPreparation import getValidation, getTrain, plotResaluts
+from  DataPreparation import getValidation, getTrain
 from DenseNetImplementation import DenceNetClassifier
-import numpy as np
 import json
 
 
-train_data, train_labels = getTrain()
-validation_data = getValidation()
+print("Data Preperation")
+train_data, train_labels = getTrain(12000)
+validation_data = getValidation(5000)
 
 model = DenceNetClassifier()
 model.compile()
 print()
+
 # ## Train the top layer (for classifaction) 
 print("Training the top layer (for classifaction) ")
-trainingFirst = model.fit(x = train_data, y = train_labels,  epochs = 30, validation_data = validation_data)
+trainingFirst = model.fit(x = train_data, y = train_labels,  epochs = 50, validation_data = validation_data, batch_size = 16)
 
   
-with open('trainingFirst', 'w') as convert_file:
+with open('trainingFirst_1', 'w') as convert_file:
      convert_file.write(json.dumps(trainingFirst.history))
 
-
-#plotResaluts(trainingFirst, 'loss', 'The top layer : Loss')
-#plotResaluts(trainingFirst, 'accuracy', 'The top layer : Accuracy')
 
 print()
 ## Fine - Tuning
 print("Fine - Tuning ")
 model.setTrainable(True)
-model.compile(optimizer = keras.optimizers.Adam(1e-5))
+model.compile(optimizer = keras.optimizers.Adam(1e-7)) # 1e-5 => 1e-7
 
-# #model
-trainingSecond = model.fit(x = train_data, y = train_labels,  epochs =  15, validation_data = validation_data)
+#model
+trainingSecond = model.fit(x = train_data, y = train_labels,  epochs =  5, validation_data = validation_data)
 
-with open('trainingSecond', 'w') as convert_file:
+with open('trainingSecond_1', 'w') as convert_file:
      convert_file.write(json.dumps(trainingSecond.history))
 
-#plotResaluts(trainingSecond , 'loss', 'Fine - Tuning: Loss')
-#plotResaluts(trainingSecond , 'accuracy', 'Fine - Tuning: Accuracy')
