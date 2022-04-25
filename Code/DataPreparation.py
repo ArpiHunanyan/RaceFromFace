94d9a36bd7d714fef1c5a838672f98b37a59b437
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-04-25 14:06:20 +0400

    Update DataPreparation.py

--- Code/DataPreparation.py
+++ Code/DataPreparation.py
@@ -4,21 +4,29 @@ import numpy as np
 import re
 from PIL import Image
 import matplotlib.pyplot as plt
+import skimage
 
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-def getValidation(M = 10954 - 1):
+def getValidation(M = 10954, age = False, gender = False):
     path_y = "Data/fairface_label_val.csv"
     path_data = "Data/validation_data.npy"
-    return getData(path_y , path_data, M)
+    return getData(path_y , path_data, M,  age, gender)
 
-def getTrain(M = 86744 - 1):
+def getTrain(M = 86744, age = False, gender = False):
     path_y = "Data/fairface_label_train.csv"
     path_data = "Data/train_data.npy" 
-    return getData(path_y , path_data, M)
+    return getData(path_y , path_data, M,  age, gender)
 
-def getData(path_y , path_data, M):
+def getData(path_y , path_data, M, age, gender):
+
+    usecols = ["race"]
+    if  age :
+        usecols.append('age')
+
+    if  gender :
+        usecols.append('gender')
 
     y = pd.read_csv(path_y, nrows = M , usecols = ["race"])
     print()
@@ -39,7 +47,50 @@ def getData(path_y , path_data, M):
     y =  to_categorical(y["race"])
     data = np.load(path_data, mmap_mode='r', )
     data = data[: M]
+    #data = skimage.color.rgb2gray(data)
 
+    inputArray = []
+
+    if  age :
+        ageDF = pd.read_csv(path_y, nrows = M , usecols = ['age'])
+
+        
+        cleanup_nums = {"age": {
+                        '0-2': 0,
+                        '3-9': 1,
+                        '10-19' : 2,
+                        '20-29' : 3,
+                        '30-39' : 4,
+                        '40-49' : 5,
+                        '50-59' : 6 ,
+                        '60-69' : 7 ,
+                        'more than 70' : 8,
+                        }
+        }
+        
+        ageDF = ageDF.replace(cleanup_nums)
+        inputArray.append(data)
+        inputArray.append(ageDF.values)
+
+
+    
+    if  gender :
+
+        genderDF = pd.read_csv(path_y, nrows = M , usecols = ['gender'])
+
+        
+        cleanup_nums = {"gender": {
+                        'Female': 0,
+                        'Male': 1 }
+        }
+        
+        genderDF = genderDF.replace(cleanup_nums)
+        inputArray.append(genderDF.values)
+
+
+    if  len(inputArray) > 0 :
+
+        return (inputArray, y)
 
     return (data, y)
 
