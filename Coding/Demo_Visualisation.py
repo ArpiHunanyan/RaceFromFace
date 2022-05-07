d89f570f123e8df74bfd23a28777b20c6c1ee1c9
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-07 14:01:20 +0400

    Create Demo_Visualisation.py

---
+++ Coding/Demo_Visualisation.py
@@ -0,0 +1,8 @@
+from  Visualisation import Plot
+
+plot = Plot()
+# plot.parameterBar()
+# plot.metricBar()
+#plot.scaterplotWithParameters()
+# opptions 'loss', 'accuracy', 'recall', 'precision', 'specificity_at_sensitivity'
+plot.plotResaluts(critaria = 'recall')
