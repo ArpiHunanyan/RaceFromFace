8b2354fa8a18825e82f696ca4ea72847b8d17602
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-07 14:01:38 +0400

    Update DataPreparation.py

--- Coding/DataPreparation.py
+++ Coding/DataPreparation.py
@@ -3,26 +3,33 @@
 # Email: arpi_hunanyan@edu.aua.am
 
 import pandas as pd
+import re
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import OneHotEncoder
 
 import numpy as np
 
-def getValidation(M = 10954):
+def getValidation(M = 10954, masked = False):
     path = "Data/fairface_label_val.csv"
-    return getOutput(path, M)
+    return getOutput(path, M, masked)
 
-def getTrain(M = 86744):
+def getTrain(M = 86744, masked = False):
     path = "Data/fairface_label_train.csv"
-    return getOutput(path, M)
+    return getOutput(path, M, masked)
 
-def getOutput(path, M):
+def getOutput(path, M, masked):
+
+    if masked :
+        file = "file_masked"
+    else :
+        file = "file"
+
         
-    dataframe = pd.read_csv(path , usecols = ["file", "race"], header = 0, nrows = M)
+    dataframe = pd.read_csv(path , usecols = [file, "race"], header = 0, nrows = M)
 
     # for i in np.unique(dataframe.race):
-    #     print(i, dataframe.loc[dataframe["race"] == i, "file"].count())
-    #     print(dataframe.loc[dataframe["race"] == i, "file"].count())
+    #     print(i, dataframe.loc[dataframe["race"] == i, file].count())
+    #     print(dataframe.loc[dataframe["race"] == i, file].count())
 
        
     assert len(dataframe.race.unique()) == 7, 'InvalidRaceGroupSize'
@@ -33,18 +40,22 @@ def getOutput(path, M):
     dataframe = dataframe.join(encoded)
     #clean up
     dataframe.drop("race", axis = 1, inplace = True)
-    dataframe.columns = ['file', 'Black', 'East Asian', 'Indian', 'Latino_Hispanic', 'Middle Eastern', 'Southeast Asian', 'White'] # alphabetically
+    dataframe.columns = [file, 'Black', 'East Asian', 'Indian', 'Latino_Hispanic', 'Middle Eastern', 'Southeast Asian', 'White'] # alphabetically
+
 
     # DataFrameIterator
     data = ImageDataGenerator().flow_from_dataframe(dataframe = dataframe,  
                                                     directory = 'Data',
-                                                    x_col = 'file', 
+                                                    x_col = file, 
                                                     y_col =  [ 'Black', 'East Asian', 'Indian', 'Latino_Hispanic', 'Middle Eastern', 'Southeast Asian', 'White'], 
                                                     batch_size = 16,
                                                     target_size = (224, 224),
                                                     class_mode = 'raw')
 
     return data
+
+
+
 
 
 
