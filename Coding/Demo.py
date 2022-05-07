4e389ff7e9706e059b8e428d1a8b29eff3297ade
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-07 14:01:30 +0400

    Update Demo.py

--- Coding/Demo.py
+++ Coding/Demo.py
@@ -3,13 +3,26 @@
 # Email: arpi_hunanyan@edu.aua.am
 
 from tensorflow import keras
+
 from  DataPreparation import getValidation, getTrain
 from Model import Classifier
 import json
+import os
+
+# set up
+unfreezeedLayersTraining = 10 # 10 from pre-traind model + classification layer
+learningRateTrainig = 0.001
+epochsTraining = 15
 
 
+learningRateFineTuning = 1e-7
+epochsFineTuning = 10
+
+
+
+print()
 print("Data Preperation")
-
+print()
 tarin_data = getTrain()
 validation_data  = getValidation()
 
@@ -17,8 +30,8 @@ validation_data  = getValidation()
 model = Classifier() #set pre-traind's layers freazed 
 
 
-model.unfreazeLastLayers()  # 10 layer 
-model.compile() # alpha 0.001
+model.unfreazeLastLayers(unfreezeedLayersTraining)  # 10 layer 
+model.compile(optimizer = keras.optimizers.Adam(learningRateTrainig)) # alpha 0.001
 
 # model.summary()
 
@@ -27,23 +40,30 @@ print()
 
 #  Train the top layer/s (for classifaction) 
 print("Training the top layer/s (for classifaction)  ")
-trainingFirst = model.fit(tarin_data,   epochs = 200, validation_data = validation_data)
+trainingFirst = model.fit(tarin_data,   epochs = epochsTraining, validation_data = validation_data)
 
   
-with open('trainingResults', 'w') as convert_file:
+with open('Results/trainingResultsEfficientnetV2_2', 'w') as convert_file:
      convert_file.write(json.dumps(trainingFirst.history))
-
+print()
+print("History is suuccsesfuly saved.")
+print()
+os.mkdir( "Model/TrainedModel" )
+model.save("Model/TrainedModel");
 
 print()
 ## Fine - Tuning
 print("Fine - Tuning ")
 model.setTrainable(True)
-model.compile(optimizer = keras.optimizers.Adam(1e-7)) # 1e-5 => 1e-7
+model.compile(optimizer = keras.optimizers.Adam(learningRateFineTuning)) # 1e-5 => 1e-7
 
 # model
-trainingSecond = model.fit(tarin_data , epochs =  20, validation_data = validation_data)
+trainingSecond = model.fit(tarin_data , epochs =  epochsFineTuning, validation_data = validation_data)
 
-with open('fineTunigResults', 'w') as convert_file:
+with open('Results/tuningResultsEfficientnetV2_2', 'w') as convert_file:
      convert_file.write(json.dumps(trainingSecond.history))
+print()
+print("History is suuccsesfuly saved.")
+print()
 
-model.save();
+model.save("Model/TunedModel");
