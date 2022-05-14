3057acb44d3ac2f0784fcd4cc532e583f06eef10
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-14 20:44:08 +0400

    Update Demo_Visualisation.py

--- Coding/Demo_Visualisation.py
+++ Coding/Demo_Visualisation.py
@@ -1,8 +1,11 @@
 from  Visualisation import Plot
 
 plot = Plot()
-# plot.parameterBar()
+# data = plot.table()
+# print(data.validation_accuracy )
+#plot.parameterBar()
+#'accuracy', 'recall', 'precision', 'specificity_at_sensitivity'
 # plot.metricBar()
 #plot.scaterplotWithParameters()
 # opptions 'loss', 'accuracy', 'recall', 'precision', 'specificity_at_sensitivity'
-plot.plotResaluts(critaria = 'recall')
+plot.plotResaluts( path = "Results/model 4_2 ResultsResNet50/tuning" )
