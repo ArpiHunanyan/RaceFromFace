2a917cccd6b340de1729f0ee361f3a687034daf1
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-14 20:44:05 +0400

    Update Visualisation.py

--- Coding/Visualisation.py
+++ Coding/Visualisation.py
@@ -1,28 +1,42 @@
 from inspect import trace
+from numpy import size
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class Plot:
     
-    def __init__(self, path = 'Results/benchmark_df_topLayer.csv'):
+    def __init__(self, path = 'Results/selection/benchmark_df_2.csv'):
+        self.coloring = """ np.where(self.benchmark_df["model_name"] == "EfficientNetV2B2", '#FFE873', np.where(self.benchmark_df["model_name"] == "DenseNet121", '#4B8BBE', "#BFC2C5")) """
+        # self.coloring = """ np.where(self.benchmark_df["model_name"] == "EfficientNetV2B2", '#FFE873', np.where(self.benchmark_df["model_name"] == "DenseNet121", '#4B8BBE', np.where( np.where( self.benchmark_df["model_name"] == "EfficientNetV2L", "#A22160", "#BFC2C5"))) """
         self.benchmark_df = pd.read_csv(path)
+        # self.benchmark_df.sort_values(["validation_accuracy" ], inplace = True, ascending = True)
+        # self.benchmark_df.to_csv("Results/benchmark_df_1.csv", index = False)
 
     def metricBar (self, x = "accuracy"):
         col = "self.benchmark_df.validation_" + x
         name = x.capitalize()
         self.benchmark_df.sort_values(["validation_" + x ], inplace = True, ascending = True)
+        self.benchmark_df.reset_index(drop=True, inplace = True)
         self.barH(col, name)
 
     def parameterBar (self):
         col = "self.benchmark_df.num_model_params" 
-        self.benchmark_df.sort_values(['num_model_params'], inplace = True, ascending = False)
-        self.barH(col,  "Paramenter")
 
-    def barH(self, col, name):
+        self.benchmark_df.sort_values(['num_model_params'], inplace = True, ascending = False)
+        self.benchmark_df.reset_index(drop=True, inplace = True)
+
+        self.barH(col = col, name = "Parameters", textVisible = False, title = " of Freezing Pre-Trained Models")
+
+    def barH(self, col, name, textVisible = True, title = " of Freezing Pre-Trained Models: 3 Epochs"):
         
         
         fig, ax = plt.subplots(figsize =(16, 8))
-        ax.barh(self.benchmark_df.model_name, eval(col), height = 0.5)
+        bar_plot = ax.barh(self.benchmark_df.model_name, eval(col), height = 0.5, color = eval(self.coloring) )
+        if textVisible:
+            for idx, rect in enumerate(bar_plot):
+                if self.benchmark_df["model_name"][idx] in ["EfficientNetV2B2", "DenseNet121"]:
+                    ax.text(np.round_(eval(col)[idx], 3) + 0.01,  rect.get_y(), np.round_(eval(col)[idx], 3), ha = 'center', rotation = 0, size = 7)
 
         for s in ['top', 'left', 'right']:
             ax.spines[s].set_visible(False)
@@ -31,7 +45,7 @@ class Plot:
         ax.xaxis.set_ticks_position('none')
         ax.yaxis.set_ticks_position('none')
         ax.xaxis.set_tick_params(pad = 5)
-        ax.yaxis.set_tick_params(pad = 10)
+        ax.yaxis.set_tick_params(pad = 0.5)
 
         ax.xaxis.grid(b = True, color = 'grey',
                 linestyle = '-.', linewidth = 0.5,
@@ -39,14 +53,25 @@ class Plot:
 
 
         plt.xlabel( name )
-        plt.ylabel('Models')
-        plt.title("Validation " +  name + "after 3 Epochs with Freezed the convolutional base.")
-        plt.legend(bbox_to_anchor=(1, 1), loc='upper left');
+        plt.ylabel('Model')
+        plt.title( name + title, pad = 10, size = 15)
+    
         plt.show()
 
-    def scaterplotWithParameters(self, x = "accuracy"):
+    def scaterplot(self, y = "accuracy", x = "params"):
         fig, ax = plt.subplots(figsize =(16, 9))
-        ax.scatter(self.benchmark_df.num_model_params, eval("self.benchmark_df.validation_" + x))
+        scatter_plot = ax.scatter(self.benchmark_df.num_model_, eval("self.benchmark_df.validation_" + y), color = eval(self.coloring))
+        
+        for i, txt in enumerate(self.benchmark_df["model_name"]):
+            if self.benchmark_df["model_name"][i] in ["EfficientNetV2B2", "DenseNet121"]:
+                ax.annotate(txt, (self.benchmark_df.num_model_params[i], (eval("self.benchmark_df.validation_" + y)[i] + 0.005)), ha = 'center', rotation = 0, size = 6)
+
+        ax.xaxis.set_ticks_position('none')
+        ax.yaxis.set_ticks_position('none')
+        ax.xaxis.set_tick_params(pad = 5)
+        ax.yaxis.set_tick_params(pad = 0.55)
+
+
         for s in ['top', 'right']:
             ax.spines[s].set_visible(False)
             ax.grid(b = True, color ='grey',
@@ -54,16 +79,15 @@ class Plot:
                     alpha = 0.5)
 
             plt.xscale('log')
-            plt.xlabel('Number of Parameters in Model')
-            plt.ylabel('Validation Accuracy after 3 Epochs')
-            plt.title('Accuracy vs Model Size')
-            plt.legend(bbox_to_anchor=(1, 1), loc='upper left');
+            plt.xlabel('Parameters',  size = 10)
+            plt.ylabel('Accuracy',  size = 10)
+            plt.title('Accuracy vs Model Size: 3 Epochs', pad = 10, size = 15)
             plt.show()
 
 
-    def plotResaluts(path = "Results/trainingResults_efficientnetv2_1", critaria = "accuracy"): 
+    def plotResaluts(self, path, critaria = "accuracy"): 
 
-        with open("Results/trainingResults_efficientnetv2_1", 'r') as training:
+        with open(path, 'r') as training:
             training = eval(training.read().splitlines()[0])
 
 
@@ -71,12 +95,19 @@ class Plot:
         plt.plot(training["val_" + critaria], label = 'validatin') # shows overfitting
 
         plt.grid(color = 'grey', linewidth = 0.5, linestyle = '-.', alpha = 0.2, b = True)
-        plt.xlabel(critaria)
-        plt.ylabel("score")
+        plt.xlabel("epoch",  size = 15 )
+        plt.ylabel(critaria, size = 15)
 
         plt.legend();
-        plt.title(label = "Trainig " + critaria + " vs Validation " + critaria , pad = 30)
+        plt.title(label = "Trainig " + critaria + " vs Validation " + critaria , pad = 30, size = 30)
         plt.show() 
+
+    def table(self, x = "accuracy") :
+
+        self.benchmark_df.sort_values(["validation_" + x ], inplace = True, ascending = False)
+        print(self.benchmark_df.head())
+    
+
 
 
 
