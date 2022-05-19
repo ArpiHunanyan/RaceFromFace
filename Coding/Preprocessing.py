bc486cc8ed2708a2880bd6833c634deb6679090b
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-19 23:19:28 +0400

    Update Preprocessing.py

--- Coding/Preprocessing.py
+++ Coding/Preprocessing.py
@@ -1,6 +1,13 @@
-import pandas as pd
-from tqdm import tqdm
-from Model import kerasModelNames, Classifier
+################ MUTED CONTEND #####################
+
+
+
+
+
+
+# import pandas as pd
+# from tqdm import tqdm
+# from Model import kerasModelNames, Classifier
 
 ## adding colums for mask images
 # df = pd.read_csv("Data/fairface_label_val.csv", header = 0)
@@ -23,22 +30,27 @@ from Model import kerasModelNames, Classifier
 
 
 
-# # model layers addition
-# path = 'Results/benchmark_df_1.csv';
-# # path = 'Results/benchmark_df_2.csv'
-# model_names = kerasModelNames()
+# # # model layers addition
+# path = 'Results/selection/benchmark_df.csv'
+
+# # model_names = kerasModelNames()
 # df = pd.read_csv(path)
-# df["num_model_layers"] = 0
+# # df["num_model_layers"] = 0
 
-# model_names = kerasModelNames()
-# for model_name in tqdm(model_names):
-#         #  "NASNetLarge" requires input images with size (331,331)
-#         if 'NASNetLarge' in model_name:
-#             continue 
+# # model_names = kerasModelNames()
+# # for model_name in tqdm(model_names):
+# #         #  "NASNetLarge" requires input images with size (331,331)
+# #         if 'NASNetLarge' in model_name:
+# #             continue 
 
-#         clf_model = Classifier( modelName = model_name)
-#         print(clf_model.baseModelLayersCount())
-#         df["num_model_layers"][df["model_name"] ==  model_name] = clf_model.baseModelLayersCount()
+# #         clf_model = Classifier( modelName = model_name)
+# #         print(clf_model.baseModelLayersCount())
+# #         df["num_model_layers"][df["model_name"] ==  model_name] = clf_model.baseModelLayersCount()
+# # df.columns = df.columns.str.replace('num_model_params', 'num_model_parameters')
+# # df.columns = df.columns.str.replace("validation_accuracy", 'accuracy')
+# # df.columns = df.columns.str.replace('val_recall', 'recall')
+# # df.columns = df.columns.str.replace('val_specificity_at_sensitivity', 'specificity at sensitivity')
+# df.columns = df.columns.str.replace( 'specificity at sensitivity', 'specificity')
 # print(df.head())
 # df.to_csv('Results/benchmark_df_2.csv', index = False)
 
